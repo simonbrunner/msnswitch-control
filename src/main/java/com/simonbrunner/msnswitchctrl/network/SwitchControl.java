66f4c6c8ef2bbe99e157b85b93ba63f4af8b47a6
Simon Brunner <mail@simonbrunner.com>
2016-01-09 16:48:36 +0100

    networking now working (although using the nashorn engine)

--- src/main/java/com/simonbrunner/msnswitchctrl/network/SwitchControl.java
+++ src/main/java/com/simonbrunner/msnswitchctrl/network/SwitchControl.java
@@ -34,8 +34,21 @@ public class SwitchControl {
             char plug2 = responseWithoutPrefix.charAt(2);
             log.info("Status Plug 1: {}", plug1);
             log.info("Status Plug 2: {}", plug2);
-            switchStatus.setPlug1(Boolean.parseBoolean(Character.toString(plug1)));
-            switchStatus.setPlug2(Boolean.parseBoolean(Character.toString(plug2)));
+
+            if (plug1 == '1') {
+                switchStatus.setPlug1(Boolean.TRUE);
+            } else {
+                switchStatus.setPlug1(Boolean.FALSE);
+            }
+
+            if (plug2 == '1') {
+                switchStatus.setPlug2(Boolean.TRUE);
+            } else {
+                switchStatus.setPlug2(Boolean.FALSE);
+            }
+
+            log.info("Parsed Status Plug 1: {}", switchStatus.getPlug1());
+            log.info("Parsed Status Plug 2: {}", switchStatus.getPlug2());
         }
 
         return switchStatus;
